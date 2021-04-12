1f07f2283d9f9d6f013e5a5a306e57ffebba670f
Jean-Raphael Poulin Arguin <jpoulina@fondsftq.com>
2021-04-12 17:32:09 -0400

    Add analytics and privacy respecting tracking

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,2 +1,31 @@
 import storeWrapper from "./storeWrapper"
+import React from "react";
+
+const PANELBEAR_SITE_ID = "CyWxIjwz3Ca";
+
+const PANELBEAR_CONFIG = {
+  site: PANELBEAR_SITE_ID,
+  spaMode: "history",
+  debug: false,
+};
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      key="panelbear-analytics-src"
+      async
+      src={`https://cdn.panelbear.com/analytics.js?site=${PANELBEAR_SITE_ID}`}
+    />,
+    <script
+      key="panelbear-analytics-code"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.panelbear = window.panelbear || function() { (window.panelbear.q = window.panelbear.q || []).push(arguments); };
+          panelbear('config', ${JSON.stringify(PANELBEAR_CONFIG)});
+        `,
+      }}
+    />,
+  ]);
+};
+
 export const wrapRootElement = storeWrapper
